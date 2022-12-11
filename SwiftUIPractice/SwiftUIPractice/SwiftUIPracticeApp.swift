1fbadb071c319ba4878d92d326bd243109097864
pkh <toby369369@naver.com>
2022-12-11 20:40:04 +0900

    :construction: SwiftUI 프로젝트 생성

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Kyoon Ho Park on 2022/12/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
